35cad6f9fa81df76fff9b16e5432c934b3658a5c
Andrei Stoicescu <andrei4002@gmail.com>
2017-03-23 11:31:33 +0200

    Initial Commit

---
+++ StatusBarTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StatusBarTest
+//
+//  Created by Andrei Stoicescu on 23/03/2017.
+//  Copyright © 2017 Andrei Stoicescu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StatusBarTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StatusBarTest
+//
+//  Created by Andrei Stoicescu on 23/03/2017.
+//  Copyright © 2017 Andrei Stoicescu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
